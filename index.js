bfbc2aef6ca93452ffec683fbc3c48c424f135cf
oo 280284 <olivier.mauge@cfa-afti.fr>
2026-01-20 18:19:15 +0100

    test

---
+++ index.js
@@ -0,0 +1,14 @@
+function evaluerChaine(chaine) {
+  try {
+    // eval() évalue la chaine de caractères comme du JavaScript
+    let resultat = eval(chaine); // Fonction à ne pas utiliser !!!
+    console.log("Résultat de l'évaluation :", resultat);
+    return resultat;
+  } catch (error) {
+    console.error("Erreur lors de l'évaluation de la chaine :", error);
+    return null;
+  }
+}
+
+// Exemple d'utilisation :
+evaluerChaine("10 + 2"); // Évalue une expression mathématique
